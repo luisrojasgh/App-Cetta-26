f00420d401f5073fac4f6f6366a79e84e4257a4b
luisrojasgh <luisvillegastlx@gmail.com>
2023-12-27 16:11:42 -0500

    Slider Ok.
    Listo el slider con modelo. vista y renderización en index.html.

--- base/views.py
+++ base/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
+from configuracion.models import Slider
 
 def principal(request):
     titulo="Bienvenid@s"
-    # sliders= Slider.objects.filter(estado=True)
+    sliders= Slider.objects.filter(estado=True)
     # productos= Producto.objects.all()
     context={
         "titulo": titulo,
-        # "sliders": sliders,
+        "sliders": sliders,
         # "productos":productos 
     }
     return render(request, "index.html", context)
