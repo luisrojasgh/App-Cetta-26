f815d881203548cf8796903c2ab1fb14f0de18f7
luisrojasgh <luisvillegastlx@gmail.com>
2024-01-11 16:47:08 -0500

    Exhibiones Ok.
    Se completan todas exhibiciones de los productos.

--- exhibicion/urls.py
+++ exhibicion/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
-from exhibicion.views import productos_mostrar, productos_mostrar_descuentos
+from exhibicion.views import productos_mostrar, productos_mostrar_descuentos, productos_mostrar_calzado, productos_mostrar_vestidos, productos_mostrar_jeans
 
 urlpatterns = [
     path('productos/',productos_mostrar,name="productos"),
-    path('descuentos/',productos_mostrar_descuentos,name="descuentos")
+    path('descuentos/',productos_mostrar_descuentos,name="descuentos"),
+    path('calzado/',productos_mostrar_calzado,name="calzado"),
+    path('vestidos/',productos_mostrar_vestidos,name="vestidos"),
+    path('jeans/',productos_mostrar_jeans,name="jeans")
 ]
--- exhibicion/views.py
+++ exhibicion/views.py
@@ -21,3 +21,33 @@ def productos_mostrar_descuentos(request):
         "productos_descuento":productos_descuento 
     }
     return render(request, "descuentos.html", context)
+
+# Función para renderizar los productos de Calzado.
+def productos_mostrar_calzado(request):
+    titulo="Calzado"
+    productos_calzado = Producto.objects.filter(categoria='CALZADO', estado=True)
+    context={
+        "titulo": titulo,
+        "productos_calzado":productos_calzado
+    }
+    return render(request, "calzado.html", context)
+
+# Función para renderizar los productos de Vestidos.
+def productos_mostrar_vestidos(request):
+    titulo="Vestidos"
+    productos_vestidos = Producto.objects.filter(categoria='VESTIDOS', estado=True)
+    context={
+        "titulo": titulo,
+        "productos_vestidos":productos_vestidos
+    }
+    return render(request, "vestidos.html", context)
+
+# Función para renderizar los productos de Jeans.
+def productos_mostrar_jeans(request):
+    titulo="Jeans"
+    productos_jeans = Producto.objects.filter(categoria='JEANS', estado=True)
+    context={
+        "titulo": titulo,
+        "productos_jeans":productos_jeans
+    }
+    return render(request, "jeans.html", context)
