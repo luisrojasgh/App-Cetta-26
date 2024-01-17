0e9d0756778b1ed07156ca039e2bb466ffe85427
luisrojasgh <luisvillegastlx@gmail.com>
2024-01-17 16:29:47 -0500

    Formulario de registro.
    Formulario de registro, versión 1.0

---
+++ comercial/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.forms import ModelChoiceField, ModelForm, widgets
+from .models import Usuario
+from django.forms import Widget
+from django.contrib.auth.models import Group, Permission
+from django.contrib.admin.widgets import FilteredSelectMultiple
+
+
+class UsuarioForm(ModelForm):
+    rol= ModelChoiceField(
+        queryset=Group.objects.all(),
+        label="Rol",
+    )
+    class Meta:
+        model= Usuario
+        fields= "__all__"
+        exclude=["estado","user"]
+        """ widgets={
+            'fecha_nacimiento':Widget.DateInput(attrs={'type':'date'},format='%Y-%m-%d')
+        }  """
--- comercial/views.py
+++ comercial/views.py
@@ -1,11 +1,61 @@
 from django.shortcuts import render, redirect
 from .models import Direccion, Usuario
-
+from .forms import UsuarioForm
+from django.contrib.auth.models import User, Group
 # Create your views here.
+
+#@permission_required('comunidad.add_usuario', raise_exception=True)
 def usuario_crear(request):
     titulo="Registro"
-    
+    accion="Registrarme"
+    usuarios= Usuario.objects.all()
+    if request.method=="POST":
+        form= UsuarioForm(request.POST)
+        if form.is_valid():
+            if not User.objects.filter(username=request.POST['documento']):
+                user = User.objects.create_user('nombre','email@email','pass')
+                user.username= request.POST['documento']
+                user.first_name= request.POST['primer_nombre']
+                user.last_name= request.POST['primer_apellido']
+                user.email= request.POST['correo']
+                #user.password=make_password("@" + request.POST['primer_nombre'][0] + request.POST['primer_apellido'][0] + request.POST['documento'][-4:])
+                user.save()
+            else:
+                user=User.objects.get(username=request.POST['documento'])
+            rol_id = request.POST.get('rol')  # Obtén el ID del grupo seleccionado en el formulario
+            if rol_id:
+                rol = Group.objects.get(id=rol_id)
+                user.groups.add(rol)  # Asocia el usuario al grupo
+            usuario = Usuario.objects.create(
+                nombre=request.POST['nombre'],
+                apellidos=request.POST['apellidos'],
+                tipo_documento=request.POST['tipo_documento'],
+                documento=request.POST['documento'],
+                correo=request.POST['correo'],
+                celular=request.POST['celular'],
+                telefono_fijo=request.POST['telefono_fijo'],
+                password=request.POST['password'],
+                direccion=request.POST['direccion'],
+                user=user,
+                
+            )
+            #messages.success(request, f'¡El Usuario se agregó de forma exitosa!')
+            """ if usuario.imagen:
+                 img = Image.open(usuario.imagen.path)
+                 img= img.resize((500,500))
+                 img.save(usuario.imagen.path) """
+            usuario.save()
+            return redirect('usuarios')
+
+        else:
+            #messages.success(request, f'¡Error al agregar al Usuario!')
+            form = UsuarioForm(request.POST)
+    else:
+        form=UsuarioForm()
     context={
-        "titulo": titulo,
+        "titulo":titulo,
+        "usuarios":usuarios,
+        "form":form,
+        "accion":accion
     }
-    return render(request, "comercial/registro.html", context)
+    return render(request,"comercial/registro.html", context)
